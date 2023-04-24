12bb5698515a8095b9795af4bd0f2792217df16e
Romain <romain.fer@hotmail.fr>
2023-04-24 17:04:43 +0400

    ⚡ PERFORMANCE: add pattern

--- R/obs_data.R
+++ R/obs_data.R
@@ -184,7 +184,7 @@ copernicus_download_api <- function(path_tab_param,
 
     }else{
         message("Skip manually 'obs_data' target (copernicus_download_api function)")
-        return(list.files(here::here("output", "data_copernicus"), full.name = TRUE))
+        return(list.files(here::here("output", "data_copernicus"), pattern = ".nc$", full.name = TRUE))
         }
 
 }
